2220cf76cc78c7ff4199a6d230b2d49d2ffe2fa3
Eric Lou <lwc_evale@hotmail.com>
2014-02-19 21:43:49 +0800

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,21 +10,24 @@ class User < ActiveRecord::Base
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, format: { with: VALID_EMAIL_REGEX, :message => '邮箱格式不正确' },
 		uniqueness: { case_sensitive: false }
-	validates :phone, format: { with: /\A\+?[\d]*-?[\d]+\z/, message: ' 电话只能输入数字'}
+	validates :phone, format: { with: /\A\+?[\d]*-?[\d]*\z/, message: ' 电话只能输入数字'}
+
+	validates_presence_of :password_confirmation, :within=>6..16, message:'新密码长度不正确，应该在6到16位之间', if: lambda { |m| m.password.present? }
+	#validates_confirmation_of :password
+	#validates_length_of :password, :within => 6..16, message: '新密码长度不正确，应该在6到16位之间', on: :create
 	
-	validates_length_of :password, :within => 6..16, message: '新密码长度不正确，应该在6到16位之间', on: :create
-	
+	# rails自带
+	has_secure_password
+
 	# 修改密码
 	attr_reader :current_password
 	def update_password(user_params)
 		current_password = user_params.delete(:current_password)
 		
+		p "@@@@@@@@@@#{self.password.present?}"
+
 		if self.authenticate(current_password)
-			if validates_of_update_password
-				self.update(user_params)
-			else
-				false
-			end
+			self.update(user_params)
 		else
 			self.errors.add(:current_password, current_password.blank? ? '旧密码不能为空' : '旧密码输入不正确')
 			false
@@ -40,8 +43,7 @@ class User < ActiveRecord::Base
 		end
 	end
 	
-	# rails自带
-	has_secure_password
+	
 	
 	def User.new_remember_token
 		SecureRandom.urlsafe_base64
