2e16eed6a51f226dd1b472b51487dcabf5b76a80
Eric Lou <lwc_evale@hotmail.com>
2014-06-06 23:04:15 +0800

    调整缩略头像的大小

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -30,7 +30,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [50, 50]
+    process :resize_to_fit => [40, 40]
   end
 
   version :medium do
