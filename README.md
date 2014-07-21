ea3e8b395a50a8ff6e966e3314db450ab1f56a63
sunday35034 <lwc_evale@hotmail.com>
2014-07-21 17:10:44 +0800

    change nprogress-bar color; use db:seed, instead of db:init

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ bundle install
 cd config
 cp config.default.yml config.yml # 将参数修改成自己的参数(注意修改faye token)
 bundle exec rake db:schema:load
-bundle exec rake db:init # 创建一个管理员用户admin@g.com/qwe123
+bundle exec rake db:seed # 创建一个管理员用户admin@g.com/qwe123
 ```
 
 注意修改`config/environments`下各环境的邮箱配置，用来发送忘记密码邮件
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name: "SuperAdmin",
+             email: "admin@g.com",
+             password: "qwe123",
+             password_confirmation: "qwe123",
+             role: "admin")
